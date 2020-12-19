99ac1a313a222fd57109abea3900f70c0c1d70c3
Ahmad Waleed <a.waleedothman@gmail.com>
2020-12-19 18:20:42 +0200

    Sign-in

--- front-end/src/components/SignIn.vue
+++ front-end/src/components/SignIn.vue
@@ -1,9 +1,47 @@
 <template>
-<div>
-  <h1>SignIn</h1>
-  <v-btn @click="signIn" class = "btn">Go To Home</v-btn>
+  <div id="signInComponent">
+    <v-card outlined>
+      <v-card-title>
+        <h3>Sign-in</h3>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-text-field v-model="email"
+                            filled
+                            label="Email"
+                            append-icon="mdi-close"
+                            @click:append="email = ''"
+                            :rules="[rules.required]"
+              ></v-text-field>
+            </v-row>
 
-</div>
+            <v-row>
+              <v-text-field filled
+                            label="Password"
+                            v-model="password"
+                            :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                            :type="show ? 'text' : 'password'"
+                            @click:append="show = !show"
+                            :rules="[rules.required]"
+              ></v-text-field>
+            </v-row>
+
+          </v-container>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-container class="btnContainer">
+            <p v-show="!valid" style="color: indianred">Oops! The email or password seems to be incorrect.<br>
+              Try again or <a @click="signUp" class="link">Sign-up instead</a>.</p>
+          <v-btn color="primary" text @click="signIn">Sign-in</v-btn>
+        </v-container>
+      </v-card-actions>
+    </v-card>
+
+  </div>
 </template>
 
 <script>
@@ -12,17 +50,47 @@
 export default {
   name: "SignIn",
 
+  data() {
+    return {
+      email: "",
+      password: "",
+      show: false,
+      valid: true,
+      rules: {
+        required: value => !!value || 'Required',
+      },
+    }
+  },
+
   methods: {
     signIn() {
-      window.location.href = 'home.html';
+      if (this.password === "admin")
+        window.location.href = 'home.html';
+      else
+        this.valid = false;
+    },
+
+    signUp(){
+      this.$router.push("/signup");
     }
-  }
+  },
+
 }
 </script>
 
 <style scoped>
 
-.btn {
-  margin: 2%;
+#signInComponent {
+  width: 30%;
+}
+
+.btnContainer {
+  margin-top: -10%;
+}
+
+.link {
+  color: indianred;
+  text-decoration: underline;
+  text-underline: indianred;
 }
 </style>
--- front-end/src/pages/signIn/App.vue
+++ front-end/src/pages/signIn/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Sign In</router-link> |
-      <router-link to="/signup">Sign Up</router-link>
-    </div>
-    <router-view/>
+    <v-app>
+      <v-main style="background-color: #F8F8F7">
+        <div id="nav">
+          <router-link to="/">Sign-in</router-link>
+          |
+          <router-link to="/signup">Sign-up</router-link>
+        </div>
+        <router-view class="centered"/>
+      </v-main>
+    </v-app>
   </div>
 </template>
 
@@ -14,19 +19,27 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
+  text-align: right;
 
   a {
     font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #039BE5;
     }
   }
+}
+
+.centered {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  /* bring your own prefixes */
+  transform: translate(-50%, -50%);
 }
 </style>
